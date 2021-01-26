904202afe3ecb2dbc0d6207ed2bdb5d4baacd02e
Steve Streeting <steve@stevestreeting.com>
2021-01-26 17:33:00 +0000

    Fix a couple of hardcoded paths

--- Source/SPUDExamples/SPUDExamplesGameMode.cpp
+++ Source/SPUDExamples/SPUDExamplesGameMode.cpp
@@ -9,7 +9,7 @@ ASPUDExamplesGameMode::ASPUDExamplesGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class

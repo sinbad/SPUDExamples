78e8a22b4b3d3c837a01c937aa0c707f2da4048e
Steve Streeting <steve@stevestreeting.com>
2021-01-27 17:57:59 +0000

    Add a BP which is solely there to test all the saveable types of property

---
+++ Source/SPUDExamples/SPUDExamplesCustomStruct.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "SPUDExamplesCustomStruct.generated.h"
+/// A custom struct just to show these can be stored too (although NOT in arrays right now, builtin structs can be)
+USTRUCT(BlueprintType)
+struct FSpudExampleCustomStruct
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, SaveGame)
+	int IntNestedField;
+	
+	UPROPERTY(EditAnywhere, SaveGame)
+	FString StringNestedField;
+	
+};

b7f2f918925c8b9fc157500c53e8029952fd3b79
Steve Streeting <steve@stevestreeting.com>
2021-05-26 15:57:43 +0100

    Include nested UObject test

---
+++ Source/SPUDExamples/SPUDExamplesNestedUObject.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SPUDExamplesNestedUObject.generated.h"
+
+/**
+ * This is a test of a nested UObject to prove it can be saved. Doesn't need to be an ISpudObject because
+ * it will be discovered inside another object that's already tracked.
+ */
+UCLASS(Blueprintable)
+class SPUDEXAMPLES_API USPUDExamplesNestedUObject : public UObject
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(SaveGame, BlueprintReadWrite)
+	int TestSubObjectProperty;
+};

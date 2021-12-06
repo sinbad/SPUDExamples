85450425df0d0fc6f0644d2cda50a5baacf64fca
Steve Streeting <steve@stevestreeting.com>
2021-12-06 11:33:55 +0000

    Added a test case for respawning pawns that opt in via AlwaysRespawn

---
+++ Source/SPUDExamples/SPUDExamplesSpawnedPawn.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ISpudObject.h"
+#include "SpudHelpers.h"
+#include "SPUDExamplesSpawnedPawn.generated.h"
+
+/**
+ * Pawn to test always respawn
+ */
+UCLASS(Blueprintable, BlueprintType)
+class SPUDEXAMPLES_API ASPUDExamplesSpawnedPawn : public ASpudPawnBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual ESpudRespawnMode GetSpudRespawnMode_Implementation() const override { return ESpudRespawnMode::AlwaysRespawn; }
+};
